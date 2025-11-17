deb802894a5c2fdcf063be1fa07dff5685df5185
ivan <9spotzs@gmail.com>
2025-11-17 11:50:17 -0500

    commit final

--- src/main/java/controlador/ControladorFacturacion.java
+++ src/main/java/controlador/ControladorFacturacion.java
@@ -1,5 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
 package controlador;
 
+/**
+ *
+ * @author 9spot
+ */
 import modelo.Cliente;
 import modelo.Factura;
 import java.util.ArrayList;
